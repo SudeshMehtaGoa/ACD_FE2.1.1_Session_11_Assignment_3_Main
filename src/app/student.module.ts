e3ecdec641e4cb344beca45f69348f364a4a0401
Mehta Sudesh <Mehta@iazi.ch>
2017-07-29 02:34:13 +0200

    session 11 assignment 3

---
+++ src/app/student.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { CommonModule } from '@angular/common';
+
+
+import { StudentComponent } from 'app/component/student-component/student.component';
+import { FirstCharUpCasePipe } from 'app/pipe/first-char-up-case.pipe';
+
+
+
+@NgModule({
+  declarations: [
+    StudentComponent,
+    FirstCharUpCasePipe
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpModule,
+    CommonModule
+  ],
+  providers: [],
+  bootstrap: [StudentComponent]
+})
+export class StudentModule { }
